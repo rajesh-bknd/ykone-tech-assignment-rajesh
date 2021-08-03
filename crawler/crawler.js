1b44e512cc3391e756f2819495c70a9a7a4da1ee
rajeshkannanvnc <rajesh.kannan@vncservices.in>
2021-08-03 20:33:29 +0530

    Crawler: company basic details and status parse - add

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -9,15 +9,61 @@ const loadHomePage = async (url) => {
     if (error) {
         throw error
     }
-    console.log(response)
     if (response && response.status === 200) {
-        const clientList = parseClientsList(response.data)
-
+        let clientList = parseClientsList(response.data)
+        clientList = clientList.slice(0, 1)
+        clientList = await clientList.map(async client => {
+            const profileInfo = await parseClientProfileInfo(client.profileUrl)
+            console.log(profileInfo)
+        })
     }
 }
 const parseClientProfileInfo = async (profileUrl) => {
-    const [response,error] = await handlePromise(axios.get(profileUrl))
+    const [response, error] = await handlePromise(axios.get(profileUrl))
+    if (error) {
+        throw error
+    }
+    if (response && response.status === 200) {
+        const profileInfo = {}
+        const $ = cheerio.load(response.data)
 
+        /*profileInfo["description"] = $(`#form1 > div > div > div > div > div > div > div > div.col-sm-12`).first().text()
+        profileInfo["description"] = profileInfo["description"].toString().trim().replace(/\n/g, " ");
+       */
+
+        profileInfo["name"] = $(`#basic_details > tbody > tr:nth-child(1) > td`).first().text()
+        profileInfo["name"] = profileInfo["name"].toString().trim()
+
+        profileInfo["activity"] = $(`#basic_details > tbody > tr:nth-child(2) > td`).first().text()
+        profileInfo["activity"] = profileInfo["activity"].toString().trim()
+
+        profileInfo["CIN"] = $(`#basic_details > tbody > tr:nth-child(3) > td`).first().text()
+        profileInfo["CIN"] = profileInfo["CIN"].toString().trim()
+
+        profileInfo["registrationDate"] = $(`#basic_details > tbody > tr:nth-child(4) > td`).first().text()
+        profileInfo["registrationDate"] = profileInfo["registrationDate"].toString().trim()
+
+        profileInfo["category"] = $(`#basic_details > tbody > tr:nth-child(5) > td`).first().text()
+        profileInfo["category"] = profileInfo["category"].toString().trim()
+
+        profileInfo["subCategory"] = $(`#basic_details > tbody > tr:nth-child(6) > td`).first().text()
+        profileInfo["subCategory"] = profileInfo["subCategory"].toString().trim()
+
+        profileInfo["class"] = $(`#basic_details > tbody > tr:nth-child(7) > td`).first().text()
+        profileInfo["class"] = profileInfo["class"].toString().trim()
+
+        profileInfo["ROC"] = $(`#company_status > tbody > tr:nth-child(1) > td`).first().text()
+        profileInfo["ROC"] = profileInfo["ROC"].toString().trim()
+
+        profileInfo["status"] = $(`#company_status > tbody > tr:nth-child(2) > td > span`).first().text()
+        profileInfo["status"] = profileInfo["status"].toString().trim()
+
+        profileInfo["isCompanyListed"] = $(`#company_status > tbody > tr:nth-child(3) > td`).first().text()
+        profileInfo["isCompanyListed"] = profileInfo["isCompanyListed"].toString().trim()
+        console.log(profileInfo)
+
+
+    }
 }
 /**
  * @param {string} html
